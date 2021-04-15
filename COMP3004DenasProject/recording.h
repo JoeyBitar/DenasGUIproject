add72eeeb4aec3d710768d295cde9303194e4116
JoeyBitar <joseph_tar@hotmail.com>
2021-04-15 19:41:35 -0400

    Added frequency for the recordings

--- COMP3004DenasProject/recording.h
+++ COMP3004DenasProject/recording.h
@@ -11,16 +11,18 @@ class Recording : public QObject
     Q_OBJECT
 public:
     explicit Recording(QObject *parent = nullptr);
-    Recording(QDateTime,QString,int,QString);       //constructor
+    Recording(QDateTime,QString,int,QString,int);       //constructor
     QString getDateTime();                          //Getter for date and time
     QString getProgram();                           //Getter for program name
     QString getMaxPower();                          //Getter for max power
     QString getDuration();                          //Getter for duration
+    QString getFrequency();
 private:
     QDateTime dateTime;                             //Date obj
     QString program;                                //Recording name
     int maxPower;                                   //Recording power
     QString duration;                               //Recording duration
+    int frequency;                                  //Frequency
 
 signals:
 
